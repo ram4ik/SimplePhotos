247fcaab32fd91ff16837086a728c80ccc0d0f8b
Ramill <ramill_ibragimov@icloud.com>
2023-09-07 10:56:46 +0300

    Initial Commit

---
+++ SimplePhotos/SimplePhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePhotosApp.swift
+//  SimplePhotos
+//
+//  Created by Ramill Ibragimov on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimplePhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
